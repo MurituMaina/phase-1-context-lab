55fca4ddd6e5f435131e55d93a7ce77d9fccde22
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-29 16:23:53 +0300

    assignment

--- index.js
+++ index.js
@@ -1,13 +1,97 @@
-/* Your Code Here */
+// // // /* Your Code Here */
+let employeeObj;
+let createEmployeeRecord = function (emp){
+ employeeObj=
+ {
+firstName:emp[0], 
+familyName:emp[1], 
+title:emp[2], 
+payPerHour:emp[3],
+timeInEvents:[],
+timeOutEvents:[]}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+return employeeObj;
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createEmployeeRecords(userArray){
+return userArray.map((user) => createEmployeeRecord(user))
+}
+
+function createTimeInEvent(day){
+//the updated Record
+let [date,hour] =day.split(' ');
+employeeObj.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour,10),
+    date, 
+    })
+return this;
+    }
+   
+    // let createTimeInEvent = function(dateStamp){
+    //     let [date, hour] = dateStamp.split(' ')
+    
+    //     this.timeInEvents.push({
+    //         type: "TimeIn",
+    //         hour: parseInt(hour, 10),
+    //         date,
+    //     })
+    
+    //     return this
+    // }
+
+
+function createTimeOutEvent (dateStamp){
+let [date, hour] = dateStamp.split(' ')
+employeeObj.timeOutEvents.push({
+type: "TimeOut",
+hour: parseInt(hour, 10),
+date,
+})
+return employeeObj;
+}
+
+function hoursWorkedOnDate (dateWorked){
+let dayIn = employeeObj.timeInEvents.find((e)=> {
+return e.date === dateWorked
+})
+let dayOut =employeeObj.timeOutEvents.find((e) => 
+{return e.date === dateWorked})
+let hours =  (dayOut.hour - dayIn.hour)/100;
+return  hours;
+}
+
+function wagesEarnedOnDate(dateWorked){
+let employeeWage;
+let hours = hoursWorkedOnDate.call(employeeObj, dateWorked);
+employeeWage = hours * employeeObj.payPerHour;
+console.log(employeeWage)
+return employeeWage;
+
+}
+
+function findEmployeeByFirstName (employeeRecordArr,firstName){
+return employeeRecordArr.find((employee)=> {
+if(employee.firstName === firstName){
+return employee;
+};
+})
+}
+
+const calculatePayroll =function (employeeRecordArr){
+    return employeeRecordArr.reduce(function(memo, employee){
+        return memo + allWagesFor.call(employee)
+    }, 0)
+}
+
+// /*
+//  We're giving you this function. Take a look at it, you might see some usage
+//  that's new and different. That's because we're avoiding a well-known, but
+//  sneaky bug that we'll cover in the next few lessons!
+
+//  As a result, the lessons for this function will pass *and* it will be available
+//  for you to use if you need it!
+//  */
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -20,4 +104,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
